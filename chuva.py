2a7e3a05f3a02cea8fab4b3b8a14e1adf2f81493
Renato Barros <renatobarrosads@gmail.com>
2023-09-23 16:24:56 -0300

    Inicio

---
+++ chuva.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+#Cria a tela
+tamno =(960, 540)
+tela = pygame.display.set_mode(tamanho)
+
+
+#Defina o titulo da janela
+pygame.display.set_caption("ChuvaMortal")
+
+#Criar um relógio para controlar os FPS
+relogio = pygame.time.Clock()
+
+
+
